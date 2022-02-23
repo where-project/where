66698b4f9e89c4d0a13e5e108ad93d99f873450e
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-23 18:00:45 +0300

    Initial commit
    Initial commit

---
+++ src/main/java/com/where/where/WhereApplication.java
@@ -0,0 +1,13 @@
+package com.where.where;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereApplication.class, args);
+	}
+
+}
