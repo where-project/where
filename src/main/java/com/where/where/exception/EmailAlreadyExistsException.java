702e9ff0be53e129c66ccf50682c3505045bf8b8
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-25 23:16:33 +0300

    [WHERE-8]: exception-olusturulmasi

---
+++ src/main/java/com/where/where/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.where.where.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+
+    }
+}
