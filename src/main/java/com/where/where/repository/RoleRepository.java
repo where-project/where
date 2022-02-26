3c3cb6f92de2eae8a1a71cebc224f1616a544a08
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-26 15:19:42 +0300

    Update role repository

--- src/main/java/com/where/where/repository/RoleRepository.java
+++ src/main/java/com/where/where/repository/RoleRepository.java
@@ -4,5 +4,4 @@ import com.where.where.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-   // RoleRepository findByName(String name);
 }
