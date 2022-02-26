19399b94eb109db13cfebde23c4ece2fa017f6f8
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-26 15:11:42 +0300

    Entity katmanı oluşturuldu

---
+++ src/main/java/com/where/where/model/Role.java
@@ -0,0 +1,22 @@
+package com.where.where.model;
+
+import com.where.where.util.RoleName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "role")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+}
