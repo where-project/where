75c763d0ae85ddbc2627fa526c425f88bba5ccf2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-26 21:12:54 +0300

    Validation kontrolü sağlandı ve category ekleme güncellendi

--- src/main/java/com/where/where/controller/CategoryController.java
+++ src/main/java/com/where/where/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.where.where.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,34 +18,36 @@ import com.where.where.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
-	private final CategoryService categoryService;
+    private final CategoryService categoryService;
 
-	@GetMapping("/getAll")
-	public List<CategoryDto> getAll() {
-		return categoryService.getAll();
-	}
+    @GetMapping("/getAll")
+    public ResponseEntity<List<CategoryDto>> getAll() {
+        return new ResponseEntity<List<CategoryDto>>(categoryService.getAll(), HttpStatus.OK);
+    }
 
-	@PostMapping("/add")
-	public CategoryDto add(@RequestBody CategoryDto categoryDto) {
-		return categoryService.add(categoryDto);
-	}
+    @PostMapping("/add")
+    public CategoryDto add(@Valid @RequestBody CategoryDto categoryDto) {
+        return categoryService.add(categoryDto);
+    }
 
-	@GetMapping("/getById/{id}")
-	public CategoryDto getById(@RequestParam Long id) {
-		return categoryService.getById(id);
-	}
+    @GetMapping("/getById/{id}")
+    public CategoryDto getById(@Valid @RequestParam Long id) {
+        return categoryService.getById(id);
+    }
 
-	@DeleteMapping("/{id}")
-	public void delete(@RequestParam Long id) {
-		categoryService.delete(id);
-	}
+    @DeleteMapping("/{id}")
+    public void delete(@RequestParam Long id) {
+        categoryService.delete(id);
+    }
 
-	@PutMapping("/update")
-	public CategoryDto update(@RequestBody CategoryDto categoryDto) {
-		return categoryService.update(categoryDto);
-	}
+    @PutMapping("/update")
+    public CategoryDto update(@Valid @RequestBody CategoryDto categoryDto) {
+        return categoryService.update(categoryDto);
+    }
 }
