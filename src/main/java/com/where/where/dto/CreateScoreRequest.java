9991971a4b831847af3e9e8c45758af8165ba46d
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-28 17:11:59 +0300

    [Where-13]: Place validation'ları tamamlandı

--- src/main/java/com/where/where/dto/CreateScoreRequest.java
+++ src/main/java/com/where/where/dto/CreateScoreRequest.java
@@ -14,7 +14,7 @@ public class CreateScoreRequest {
     @Min(value = 1, message = "Cannot be smaller than 1")
     @Max(value = 5, message = "Cannot be bigger than 5")
     private float venueScore;
-    
+
     @Min(value = 1, message = "Cannot be smaller than 1")
     @Max(value = 5, message = "Cannot be bigger than 5")
     private float coronaScore;
