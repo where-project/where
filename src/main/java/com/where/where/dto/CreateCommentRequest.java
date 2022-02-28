ace1ba5b0230765939bf207d6ed466d5c283bcae
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-28 17:20:16 +0300

    [WHERE-13]: Comment validation tamamlandı

--- src/main/java/com/where/where/dto/CreateCommentRequest.java
+++ src/main/java/com/where/where/dto/CreateCommentRequest.java
@@ -4,12 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateCommentRequest {
-	private String commentText;
-	private String createDate;
-	private Long userId;
-	private Long placeId;
+
+    @NotNull(message = "Must be not null")
+    @Size(max = 20, min = 2, message = "Must be between 2 and 20")
+    private String commentText;
+
+    @NotNull(message = "Must be not null")
+    @NotBlank(message = "Must be not blank")
+    private String createDate;
+
+    @NotNull(message = "Must be not null")
+    private Long userId;
+
+    @NotNull(message = "Must be not null")
+    private Long placeId;
 }
