2720b3776dd0a9bfb4d0bf22f6381772a2ebe814
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-04-28 22:48:24 +0300

    Update EmailController.java

--- src/main/java/com/where/where/controller/EmailController.java
+++ src/main/java/com/where/where/controller/EmailController.java
@@ -1,21 +1,16 @@
 package com.where.where.controller;
 
-import com.where.where.model.BaseUser;
 import com.where.where.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 @RestController
 @RequestMapping("/api/v1/email-sender")
