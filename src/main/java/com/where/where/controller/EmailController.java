e13b96031e39790b5a981751fba05359ac1dede9
Yasemin Gerboğa <yasemingerboga@gmail.com>
2022-06-14 00:40:41 +0300

    bugfix email sender

--- src/main/java/com/where/where/controller/EmailController.java
+++ src/main/java/com/where/where/controller/EmailController.java
@@ -1,45 +1,49 @@
 package com.where.where.controller;
 
-import com.where.where.service.EmailService;
+import javax.mail.MessagingException;
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.mail.MessagingException;
+import com.where.where.dto.model.ReservationModel;
+import com.where.where.service.EmailService;
 
 @RestController
 @RequestMapping("/api/v1/email-sender")
 public class EmailController {
-    private Logger logger = LoggerFactory.getLogger(EmailController.class);
-    @Autowired
-    private EmailService emailService;
+	private Logger logger = LoggerFactory.getLogger(EmailController.class);
+	@Autowired
+	private EmailService emailService;
 
-    @PostMapping("/send-email")
-    public String sendEmail(@RequestParam String email) {
-        try {
-            emailService.sendEmail(email, "Merhaba bu bir Where projesi " +
-                            "mail gönderme sistemi testidir. Kaptan pilotunuz ONUR konuşmaktadır. " +
-                            "Lütfen mesaji dikkate almayınız !!!",
-                    "Payment Details WHERE");
-        } catch (MailException e) {
-            logger.info(e.getMessage());
-        }
-        return "Mail sent successfully";
-    }
+	@PostMapping("/send-email")
+	public boolean sendEmail(@Valid @RequestBody ReservationModel reservationModel) {
+		try {
+			emailService.sendEmail(reservationModel.getEmail(),
+					"The reservation you have made to the " + reservationModel.getPlaceName() + " on "
+							+ reservationModel.getReservationDate() + " at " + reservationModel.getReservationTime()
+							+ " time has been completed. You can log in with the " + "#123" + " code.",
+					"Reservation Details WHERE");
+		} catch (MailException e) {
+			logger.info(e.getMessage());
+		}
+		return true;
+	}
 
-    @PostMapping("/send-email-with-attachment")
-    public String sendEmailWithAttachment(@RequestParam String email) throws MessagingException {
-        emailService.sendEmailWithAttachment(email, "Merhaba bu bir Where projesi " +
-                        "mail gönderme sistemi testidir. Kaptan pilotunuz ONUR konuşmaktadır. " +
-                        "Lütfen mesaji dikkate almayınız !!! This email has attachment",
-                "Payment Details WHERE",
-                "C:\\\\Users\\\\onura\\\\Desktop\\\\where\\\\Email\\\\wh.png");
-        return "Mail sent successfully";
-    }
+	@PostMapping("/send-email-with-attachment/{email}")
+	public String sendEmailWithAttachment(@PathVariable String email) throws MessagingException {
+		emailService.sendEmailWithAttachment(email,
+				"Merhaba bu bir Where projesi "
+						+ "mail gönderme sistemi testidir. Kaptan pilotunuz ONUR konuşmaktadır. "
+						+ "Lütfen mesaji dikkate almayınız !!! This email has attachment",
+				"Payment Details WHERE", "C:\\\\Users\\\\onura\\\\Desktop\\\\where\\\\Email\\\\wh.png");
+		return "Mail sent successfully";
+	}
 }
-
